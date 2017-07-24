189f0db079e8da9b05ba8aa2773a70266074f44a
Borysiuk Andrey <factodessa@gmail.com>
2017-07-25 02:35:45 +0300

    footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,29 @@
 <footer>
-    <h3>footer</h3>
+
+    <div class="navbar-fixed-bottom row-fluid" id="footer">
+        <div class="navbar-inner">
+            <div class="container">
+                <ul class="list-inline">
+                    <li><a class="btn btn-block btn-social btn-bitbucket">
+                            <span class="fa fa-bitbucket"></span> ... Bitbucket
+                        </a></li>
+                    <li><a class="btn btn-block btn-social btn-dropbox">
+                            <span class="fa fa-dropbox"></span> ... Dropbox
+                        </a></li>
+                    <li> <a class="btn btn-block btn-social btn-facebook">
+                            <span class="fa fa-facebook"></span> ... Facebook
+                        </a></li>
+                    <li><a class="btn btn-block btn-social btn-google">
+                            <span class="fa fa-google"></span> ... Google
+                        </a></li>
+                    <li><a class="btn btn-block btn-social btn-linkedin">
+                            <span class="fa fa-linkedin"></span> ... LinkedIn
+                        </a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
 </footer>
 
 
