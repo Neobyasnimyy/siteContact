e9e01cd67669545373b600bbe99e4d6e7f9c4997
Borysiuk Andrey <factodessa@gmail.com>
2017-07-25 02:36:23 +0300

    сократили стили

--- templates/headAndHeader.php
+++ templates/headAndHeader.php
@@ -11,24 +11,46 @@
     <!-- 3. Подключаем скомпилированный и минимизированный файл JavaScript платформы Bootstrap 3 -->
     <script src="../../style/bootstrap/js/bootstrap.min.js"></script>
 
-    <script src="../../style/header.css"></script>
-    <script src="<?php echo $style; ?>"></script>
-    <script src="../../style/footer.css"></script>
+    <link href="../../style/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+
+    <link href="../../style/base.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo (isset($style))? $style:''; ?>">
 
     <style>
         <?php if (testAuthorization()):?>
         body{
-            background-color: <?php echo $_SESSION['Authorization']['color']?>;
+            background-color: <?php echo $_SESSION['Authorization']['fontColor']?>;
         }
        <?php endif; ?>
+
     </style>
 
 </head>
 
 <body>
 <header>
-    <div class="container">
-    <h3>header</h3>
-    </div>
-
+        <div class="navbar navbar-static-top" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="home.php">My Site</a>
+                </div>
+                <div class="navbar-collapse ">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="home.php">Главная</a></li>
+                        <li><a href="#about">Контакт</a></li>
+                        <li><a href="articles.php">Статьи</a></li>
+                        <li><a href="product.php">Продукция</a></li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <?php if ($authorization):?>
+                            <li><a href="admin.php">Здравствуйте <?php echo $_COOKIE['name']?></a></li>
+                            <li><a href="logout.php">Выйти</a></li>
+                        <?php else:?>
+                            <li><a href="register.php">Регистрация</a></li>
+                            <li><a href="login.php">Войти</a></li>
+                        <?php endif; ?>
+                    </ul>
+                </div>
+            </div>
+        </div>
 </header>
