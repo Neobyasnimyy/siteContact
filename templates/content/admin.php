e59008e4fc82cc853e8502b1c5df7c339d733f4f
Borysiuk Andrey <factodessa@gmail.com>
2017-07-24 21:10:21 +0300

    админка добавили запись в файл фонт колор

--- templates/content/admin.php
+++ templates/content/admin.php
@@ -1,7 +1,9 @@
 <?php
 
 require 'myFun/testAuthorization.php';
+
 $title = "Adminka";
+$yourfile = "register.txt";
 
 // если не зарегистрированный пользователь  перенаправляем на вход
 if (!testAuthorization()) {
@@ -10,43 +12,71 @@ if (!testAuthorization()) {
 }
 
 $colors = ['white', 'whitesmoke', 'wheat', 'lightyellow', 'lightskyblue', 'burlywood'];
-
-if (!empty($_POST['color'])===true) {
-    $_SESSION['Authorization']['color']=$_POST['color'];
+// смена фона
+if (!empty($_POST['fontColor']) === true) {
+    $_SESSION['Authorization']['fontColor'] = $_POST['fontColor'];
+    // проверяем доступен ли файл для записи и чтения
+    if ((is_readable($yourfile)and (is_writable($yourfile)))){
+        $file = file($yourfile);
+        $find = false; // это будет проверка нашли ли мы запись
+        foreach ($file as &$item) {
+            $item = unserialize($item);
+            if (($item['name'] === $_SESSION['Authorization']['name']) and ($item['email'] === $_SESSION['Authorization']['email'])) {
+                $item['fontColor'] = $_POST['fontColor'];
+                $find = true;
+            }
+            $item = serialize($item) . PHP_EOL;
+        }
+        if ($find==true){
+            // перезаписываем наш файл с регистрацией
+            if (file_put_contents($yourfile, implode("", $file),LOCK_EX) !== false) {
+                $message[] = "Смена фона успешно произведена.";
+            }else{
+                $message[] = "Ошибка записи данных.";
+            }
+        }
+    }else{
+        $message[] = "Ошибка записи данных";
+    }
 }
-if ((!empty($_POST['oldPassword'])===true)and(!empty($_POST['newPassword'])===true)) {
+
+// смена пароля
+if ((!empty($_POST['oldPassword']) === true) and (!empty($_POST['newPassword']) === true)) {
     $oldPassword = strip_tags($_POST['oldPassword']);
     $newPassword = strip_tags($_POST['newPassword']);
 
     $uppercase = preg_match('@[A-Z]@', $newPassword);
     $lowercase = preg_match('@[a-z]@', $newPassword);
-    $number    = preg_match('@[0-9]@', $newPassword);
-    if (empty($oldPassword) === true || empty($newPassword) === true ) {
+    $number = preg_match('@[0-9]@', $newPassword);
+    if (empty($oldPassword) === true || empty($newPassword) === true) {
         $message[] = "Каждое поле должно быть заполненно";
-    }elseif (!$uppercase || !$lowercase || !$number || strlen($newPassword) < 8){
+    } elseif (!$uppercase || !$lowercase || !$number || strlen($newPassword) < 8) {
         $message[] = "Новый пароль должен содержать cтрочные и прописные латинские буквы, цифры, спецсимволы. Минимум 8 символов";
-    }else {
-        $yourfile = "register.txt";
-        $oldstr = "Старый текст";
-        $newstr = "Новый текст";
-        $file = file($yourfile);
-        if (is_array($file))
-        {
-            foreach($file as  &$item)
-            {$item = unserialize($item);
-                if (($item['name']==$_SESSION['Authorization']['name'])and($item['password']==$oldPassword) and ($item['email']==$_SESSION['Authorization']['email'])){
-                    $item['password']= $newPassword;
+    } else {
+        // проверяем доступен ли файл для записи и чтения
+        if ((is_readable($yourfile)and (is_writable($yourfile)))){
+            $file = file($yourfile);
+            $find = false; // это будет проверка соответствия на старый пароль и на остальное
+            foreach ($file as &$item) {
+                $item = unserialize($item);
+                if (($item['name'] === $_SESSION['Authorization']['name']) and ($item['password'] === $oldPassword) and ($item['email'] === $_SESSION['Authorization']['email'])) {
+                    $item['password'] = $newPassword;
+                    $find = true;
                 }
-                $item = serialize($item).PHP_EOL;
+                $item = serialize($item) . PHP_EOL;
             }
-        }
-        else
-        {
-            exit ("Ошибка");
-        }
-       // перезаписываем наш файл с регистрацией
-        if (file_put_contents ($yourfile,implode("",$file))!==false){
-            $message[] = "Смена пароля успешно произведена.";
+            if ($find==true){
+                // перезаписываем наш файл с регистрацией
+                if (file_put_contents($yourfile, implode("", $file),LOCK_EX) !== false) {
+                    $message[] = "Смена пароля успешно произведена.";
+                }else{
+                    $message[] = "Ошибка записи данных.";
+                }
+            }else{
+                $message[] = "Ошибка ввода действующего пароля.";
+            }
+        }else{
+            $message[] = "Ошибка записи данных";
         }
     }
 }
@@ -70,16 +100,16 @@ if ((!empty($_POST['oldPassword'])===true)and(!empty($_POST['newPassword'])===tr
 
                 <div class="form-group">
                     <label for="color" class="control-label">Выбрать </label>
-                        <select  name="color" id="color" class="form-control">
-                            <?php foreach ($colors as $color): ?>
-                            <option value="<?php echo $color;?>"
-                                <?php if (isset($_SESSION['Authorization']['color']) && ($color == $_SESSION['Authorization']['color'])) {
+                    <select name="fontColor" id="color" class="form-control">
+                        <?php foreach ($colors as $color): ?>
+                            <option value="<?php echo $color; ?>"
+                                <?php if (isset($_SESSION['Authorization']['fontColor']) && ($color == $_SESSION['Authorization']['fontColor'])) {
                                     echo 'selected';
                                 } ?>>
-                                <?php echo $color;?>
+                                <?php echo $color; ?>
                             </option>
-                            <?php endforeach; ?>
-                        </select>
+                        <?php endforeach; ?>
+                    </select>
                 </div>
 
                 <div class=" form-group">
@@ -97,10 +127,10 @@ if ((!empty($_POST['oldPassword'])===true)and(!empty($_POST['newPassword'])===tr
             <h4 align="left">Поменять пароль</h4>
             <form class="form-inline" action="" method="POST" novalidate>
                 <div class="form-group">
-                    <input type="password" class="form-control" name="oldPassword" placeholder="Старый пароль" required >
+                    <input type="password" class="form-control" name="oldPassword" placeholder="Старый пароль" required>
                 </div>
                 <div class="form-group">
-                    <input type="password" class="form-control" name="newPassword" placeholder="Новый пароль" required >
+                    <input type="password" class="form-control" name="newPassword" placeholder="Новый пароль" required>
                 </div>
                 <div class=" form-group">
                     <div class=" text-right ">
